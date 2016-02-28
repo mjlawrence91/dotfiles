b01732af5cf036a2521976998ef94ad544ec9cf0
Matt Lawrence <ninja570@hotmail.com>
2016-02-28 18:53:29 +0000

    Add backup script

---
+++ .scripts/mjl-backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SRCDIR="/home/matt/"
+BASEDESTDIR="/mnt/Backup"
+EXCLUDELIST="/home/matt/mjl-backup-exclusions"
+NEWDIRNAME="$(date +"%Y-%m-%d")/"
+DIRCOUNT="$(find /mnt/Backup -mindepth 1 -maxdepth 1 -type d | wc -l)"
+DIRLIMIT=3
+
+if [[ $DIRCOUNT -ge $DIRLIMIT ]]
+then
+	echo "There are $DIRCOUNT backups already on this drive. Change drives before continuing."
+	exit 0
+else
+	echo "New backup"
+
+	mkdir -p $NEWDIRNAME
+	rsync -av -P --stats --human-readable --exclude-from=$EXCLUDELIST $SRCDIR $BASEDESTDIR/$NEWDIRNAME | pv -lep -s 3838
+fi
