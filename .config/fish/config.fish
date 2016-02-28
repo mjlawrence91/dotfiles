4f4e312d2b71a86da29840822ee979899ea092eb
Matt Lawrence <ninja570@hotmail.com>
2016-02-28 18:56:00 +0000

    Add alias for backup script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,36 +1,26 @@
-# Path to fisherman install
-set fisher_home ~/.local/share/fisherman
-set fisher_config ~/.config/fisherman
-source $fisher_home/config.fish
-set fisher_home ~/fisherman
-
 # Path to Oh My Fish install.
-set -gx OMF_PATH /home/matt/.local/share/omf
+set -gx OMF_PATH "/home/matt/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG /home/matt/.config/omf
+#set -gx OMF_CONFIG "/home/matt/.config/omf"
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
-# Load z into shell
-source $HOME/.config/z-fish/z.fish
-source $HOME/.config/fish/functions/z.fish
-source $HOME/.config/fish/completions/z.fish
-
+# Load powerline
 set fish_function_path $fish_function_path "/usr/lib/python3.5/site-packages/powerline/bindings/fish"
 powerline-setup
 
-# WORKAROUND: Tried to print invalid wide character string
-set --global --export LANG en_GB.UTF-8
-set --global --export LC_ALL en_GB.UTF-8
-
-# Ensure that numpad Enter key works
-xmodmap ~/.Xmodmap
+# Load z into shell
+source $HOME/.config/z-fish/z.fish
+source $HOME/.config/z-fish/z_complete.fish
 
 # Map ls command to something more useful
 alias ls="ls -hl --color=auto"
 alias lsa="ls -hla --color=auto"
+
+# Alias to backup routine
+alias mjlbackup="/home/matt/mjl-backup.sh"
 
 # Load Ranger rc.conf in Home directory
 set RANGER_LOAD_DEFAULT_RC=FALSE
@@ -46,8 +36,10 @@ set -g theme_avoid_ambiguous_glyphs yes
 
 # Configure greeting
 function fish_greeting
-    set_color $fish_color_autosuggestion[1]
-    uname -nsr
-    uptime
-    set_color normal
+  set_color $fish_color_autosuggestion[1]
+  uname -nsr
+  uptime
+  set_color normal
 end
+
+set -U fish_user_paths $HOME/util $fish_user_paths
