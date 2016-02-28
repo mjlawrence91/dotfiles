cb5d057e5959d826bf8e28b161ad3958bf4913ec
Matt Lawrence <ninja570@hotmail.com>
2016-02-28 19:03:58 +0000

    Remove stray echo command

--- .scripts/mjl-backup.sh
+++ .scripts/mjl-backup.sh
@@ -12,8 +12,6 @@ then
 	echo "There are $DIRCOUNT backups already on this drive. Change drives before continuing."
 	exit 0
 else
-	echo "New backup"
-
 	mkdir -p $NEWDIRNAME
 	rsync -av -P --stats --human-readable --exclude-from=$EXCLUDELIST $SRCDIR $BASEDESTDIR/$NEWDIRNAME | pv -lep -s 3838
 fi
